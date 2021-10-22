3a7f4924279abdf1cfde359f71d38adad0dae802
zimekk <zimekk@gmail.com>
2021-10-22 19:13:13 +0200

    chore: initial commit

---
+++ packages/web/src/containers/Forms.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Forms.module.scss";
+
+export default function Section() {
+  return (
+    <section className={styles.Section}>
+      <h2>Forms</h2>
+    </section>
+  );
+}
---
+++ packages/web/src/service-worker.ts
@@ -0,0 +1,7 @@
+/// <reference lib="webworker" />
+
+// https://stackoverflow.com/questions/63116331/how-to-migrate-service-worker-from-js-to-ts
+/* eslint-disable no-redeclare */
+declare var self: ServiceWorkerGlobalScope;
+
+export {};
